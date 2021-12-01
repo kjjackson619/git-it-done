8403c160288de9e0ba26bd8cc6263e82972e2d1e
KJ Jackson <kjjackson619@yahoo.com>
2021-11-30 19:36:38 -0500

    functionality

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -22,58 +22,58 @@ var getUserRepos = function (user) {
     })
         .catch(function (error) {
             alert("Unable to connect to Github");
-        });
+        })
+};
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    //get value from input
+    var username = nameInputEl.value.trim();
 
-    var formSubmitHandler = function (event) {
-        event.preventDefault();
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username");
+    }
+};
 
-        //get value from input
-        var username = nameInputEl.value.trim();
+var displayRepos = function (repos, searchTerm) {
 
-        if (username) {
-            getUserRepos(username);
-            nameInputEl.value = "";
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for (var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl);
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
-            alert("Please enter a Github username");
-        }
-    };
-
-    var displayRepos = function (repos, searchTerm) {
-
-        if (repos.length === 0) {
-            repoContainerEl.textContent = "No repositories found.";
-            return;
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
 
-        repoContainerEl.textContent = "";
-        repoSearchTerm.textContent = searchTerm;
+        repoEl.appendChild(statusEl);
 
-        for (var i = 0; i < repos.length; i++) {
-            var repoName = repos[i].owner.login + "/" + repos[i].name;
+        repoContainerEl.appendChild(repoEl);
+    }
+};
 
-            var repoEl = document.createElement("div");
-            repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-            var titleEl = document.createElement("span");
-            titleEl.textContent = repoName;
-
-            repoEl.appendChild(titleEl);
-
-            var statusEl = document.createElement("span");
-            statusEl.classList = "flex-row align-center";
-
-            if (repos[i].open_issues_count > 0) {
-                statusEl.innerHTML =
-                    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-            } else {
-                statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-            }
-
-            repoEl.appendChild(statusEl);
-
-            repoContainerEl.appendChild(repoEl);
-        }
-    };
-
-    userFormEl.addEventListener("submit", formSubmitHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);

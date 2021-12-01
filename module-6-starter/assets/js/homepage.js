cbb89a8e82d16d95c1aea86f7dcc22375171c7bc
KJ Jackson <kjjackson619@yahoo.com>
2021-11-30 23:32:34 -0500

    link files

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -4,6 +4,7 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+
 var getUserRepos = function (user) {
 
     //format github api url
@@ -51,8 +52,10 @@ var displayRepos = function (repos, searchTerm) {
     for (var i = 0; i < repos.length; i++) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
 
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,21 +1,40 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
     fetch(apiUrl).then(function (response) {
+
+
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
 
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -59,7 +78,7 @@ var displayWarning = function (repo) {
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
     var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.textContent = "Github.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
 
@@ -67,5 +86,4 @@ var displayWarning = function (repo) {
 };
 
 
-
-getRepoIssues("facebook/react");
+getRepoName();
